307ddbf59af9f7a089416ffb707af5fed2cdd9c9
dhuser <dhuser@pop-os.localdomain>
2023-03-14 16:05:56 +0800

    traced model

--- src/lid-preproc.py
+++ src/lid-preproc.py
@@ -6,6 +6,7 @@ from typing import Any, List, Optional, Union
 import torch
 import gradio as gr
 from speechbrain.pretrained import EncoderClassifier
+from TweakedEncoderClassifier import *
 
 ''' Gradio Input/Output Configurations '''
 inputs: Union[str, gr.Audio] = gr.Audio(source='upload', type='filepath')
@@ -26,19 +27,28 @@ MAP_LOCATION: str = torch.device('cuda:{}'.format(DEVICE[0]) if ACCELERATOR == '
 def initialize_lid_model(cfg: BaseConfig) -> EncoderClassifier:
 
     # lid_model = EncoderClassifier.from_hparams(source=cfg.model_source, savedir=cfg.model_dir)
-    lid_model = EncoderClassifier.from_hparams(source=cfg.model_source, savedir=cfg.model_source)
+    lid_model = TweakedEncoderClassifier.from_hparams(source=cfg.model_source, savedir=cfg.model_source)
 
     return lid_model
 
 ''' Initialize models '''
 lid_model = initialize_lid_model(config)
 
-
 ''' Main prediction function '''
-def predict(audio_path: str) -> str:
+def predict(input, wav=False) -> str:
+    if wav is False:
+        signal = lid_model.load_audio(input)
+    else:
+        signal = input
+    print(signal.shape)
+    prediction =  lid_model(signal)
+    # language = prediction[3]
 
-    signal = lid_model.load_audio(audio_path)
-    prediction =  lid_model.classify_batch(signal)
-    language = prediction[3]
+    pscore, index, language = lid_model.postproc(prediction)
 
     return [(language, 'Language')]
+
+def trace(model, output_path):
+    input = torch.rand([20800])
+    traced_model = torch.jit.trace(model, input)
+    return traced_model.save(output_path)
